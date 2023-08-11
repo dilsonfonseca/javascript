5c5bdc514cf9338bf6f21407cee09ef2ad77b417
Dilson Fonseca <135386113+dilsonfonseca@users.noreply.github.com>
2023-08-11 17:18:52 -0300

    Update ex01.js

--- softex-frontend/ex01.js
+++ softex-frontend/ex01.js
@@ -1,4 +1,4 @@
 let distancia = parseFloat(prompt("Qual a distância percorrida em metros?"));
 let tempo = parseFloat(prompt("Qual foi o tempo gasto?"));
 let velocidade = (distancia/ tempo);
-alert("A sua veelocidade foi de:" + velocidade + "metros por segundo");
+alert("A sua veelocidade foi de:" + velocidade + "metros por segundo!");
