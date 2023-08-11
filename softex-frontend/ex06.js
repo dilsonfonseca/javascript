e5c818fc56b7884a95ab7d9158d015c1e49b72e6
Dilson Fonseca <135386113+dilsonfonseca@users.noreply.github.com>
2023-08-11 19:30:58 -0300

    Create ex06.js

---
+++ softex-frontend/ex06.js
@@ -0,0 +1,13 @@
+let nome = ""
+let nota1 
+let nota2
+let nota3
+nome = prompt("Qual o seu nome?")
+nota1 = parseFloat(prompt("Digite a primeira nota:"))
+nota2 = parseFloat(prompt("Digite a segunda nota:"))
+nota3 = parseFloat(prompt("Digite a terceira nota:"))
+media = (nota1 + nota2 + nota3)/3
+alert(media);
+
+
+
