87316b7eb545f7277af3f6e4c338fa391cb9471b
Dilson Fonseca <135386113+dilsonfonseca@users.noreply.github.com>
2023-08-28 22:25:21 -0300

    .

---
+++ js/curso-em-video/aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 12
+console.log("A velocidade do seu carro é de ${vel}Km/hr")
+if (vel > 60){
+    console.log("Você está dirigindo acima da velocidade permitida, multado!")
+}
+console.log("Dirija sempre com segurança.")
