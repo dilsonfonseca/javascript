4706a5f3dc2368b68da52004c159bdc0a3b6bd3f
Dilson Fonseca <135386113+dilsonfonseca@users.noreply.github.com>
2023-08-31 23:40:02 -0300

    .

--- js/curso-em-video/ex15/script.js
+++ js/curso-em-video/ex15/script.js
@@ -9,32 +9,33 @@ function verificar(){
         let fsex = document.getElementsByName('radsex')
         let idade = ano - Number(fano.value)
         let gênero = ''
-        let img = document.createElement
+        let img = document.createElement('img')
         img.setAttribute('id','foto')
         if (fsex[0].checked) {
             gênero = 'Homem'
             if (idade >= 0 && idade < 10){
-
+                img.setAttribute('src', 'img/baby-1-m.png')
             } else if (idade < 21) {
-
+                img.setAttribute('src', 'img/teen-1-m.png')
             } else if (idade < 58) {
-
+                img.setAttribute('src', 'img/adult-1-m.png') 
             } else {
-
+                img.setAttribute('src', 'img/old-1-m.png')
             }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >= 0 && idade < 10){
-
+                img.setAttribute('src', 'img/baby-1-f.png')
             } else if (idade < 21) {
-
+                img.setAttribute('src', 'img/teen-1-f.png')
             } else if (idade < 58) {
-
+                img.setAttribute('src', 'img/adult-1-f.png') 
             } else {
-                
+                img.setAttribute('src', 'img/old-1-f.png')
             }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
