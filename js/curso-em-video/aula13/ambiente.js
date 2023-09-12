62cfab7267ad95c278b4692be7a1a2d1f2826451
Dilson <dilsonfonseca97@gmail.com>
2023-09-12 20:38:13 -0300

    Create ambiente.js

---
+++ js/curso-em-video/aula13/ambiente.js
@@ -0,0 +1,15 @@
+let contador = 0
+
+do{
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 6)
+// REPETIÇÃO COM TESTE NO FINAL
+
+
+
+
+/*while (contador <= 50) {
+    console.log(contador)
+    contador++
+} REPETIÇÃO COM TESTE NO INÍCIO*/
