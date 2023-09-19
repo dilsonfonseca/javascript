45afa04025681aaba67fe5c0374e37816d1d3619
Dilson F <dilsonfonseca97@gmail.com>
2023-09-18 21:16:41 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-for (let i = 1; i <= 100; i+=2){
-    console.log(i)
+let n1 = prompt("Digite um número")
+let n2 = prompt("Digite outro número")
+let result
+
+if (n2 > n1){
+    console.log("Escreva novamente")
+} else{
+    result = parseInt(n1/n2 * 100 + "%")
 }
+console.log(result)
