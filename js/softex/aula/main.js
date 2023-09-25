49c88db94c09092bfa7463cf42380d0d622826ab
Dilson <dilsonfonseca97@gmail.com>
2023-09-25 19:43:20 -0300

    Update main.js

--- js/softex/aula/main.js
+++ js/softex/aula/main.js
@@ -3,6 +3,17 @@ let length = texto.length
 
 console.log('O tamanho da string é : ' + length)*/
 
-let texto = "Apple, Banana, Kiwi"
+/*let texto = "Apple, Banana, Kiwi"
+let texto2 = ["Apple", "Banana", "Kiwi"]
+console.log(texto2[1])
 let parteTexto = texto.slice(7,19)
-console.log("Texto extraído: " + parteTexto)
+console.log("Texto extraído: " + parteTexto)*/
+
+/*let texto = "Please visit Microsoft!"
+let novoTexto = texto.replace("Microsoft", "W3Schools")
+console.log('Novo Texto : ' + novoTexto)*/
+
+let texto1 = "Hello World!"
+let texto2 = texto1.toUpperCase();
+
+console.log(texto2)
