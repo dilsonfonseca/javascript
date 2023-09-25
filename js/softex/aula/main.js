69a5db2d6df0d56270f76366a02ba49f10c81a47
Dilson <dilsonfonseca97@gmail.com>
2023-09-25 19:14:18 -0300

    Update main.js

--- js/softex/aula/main.js
+++ js/softex/aula/main.js
@@ -1,4 +1,8 @@
-let texto = "ABCDEFGDH"
+/*let texto = "ABCDEFGDH"
 let length = texto.length
 
-console.log('O tamanho da string é : ' + length)
+console.log('O tamanho da string é : ' + length)*/
+
+let texto = "Apple, Banana, Kiwi"
+let parteTexto = texto.slice(7,13)
+console.log("Texto extraído: " + parteTexto)
