b02ee2769175765e035fa0e25bf8227cced68fb6
Dilson <dilsonfonseca97@gmail.com>
2023-09-25 21:20:11 -0300

    Update operacoesmath.js

--- js/softex/aula/operacoesmath.js
+++ js/softex/aula/operacoesmath.js
@@ -4,9 +4,3 @@ let teste2 = Math.max(0, 150, 30, 20, -8, -200)
 console.log(teste1)
 console.log(teste2)*/
 
-let arrayNumeros = [10, 11, 22, 23, 34, 45, 66, 17, 28, 93, 101]
-let i = 0
-
-while(arrayNumeros[i] <= 11){
-    if (arrayNumeros >=)
-}
