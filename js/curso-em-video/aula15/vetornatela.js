92d788002b35e4c6542fdc79de022820c53b66a7
Dilson <dilsonfonseca97@gmail.com>
2023-09-28 22:50:59 -0300

    .

--- js/curso-em-video/aula15/vetornatela.js
+++ js/curso-em-video/aula15/vetornatela.js
@@ -1,9 +1,17 @@
 let num = [2, 5, 6, 7, 9]
-
+// USANDO FOR PARA PERCORRER ARRAY
 /*for (pos = 0; pos < num.length; pos++){
     console.log(`a posição ${pos} tem valor ${num[pos]}`)
 } */
-
+// FOR IN
 for (let pos in num){
-    console.log()
+    console.log(num[pos])
+}
+
+let pos = num.indexOf(5)
+
+if (pos == -1){
+    console.log('valor não encontrado')
+} else{
+    console.log(`o valor está na posição ${pos}`)
 }
