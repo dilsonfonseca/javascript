174f2b461dd0938a30e9df3a2877f8d1b5b589a8
Dilson <dilsonfonseca97@gmail.com>
2023-09-28 21:19:42 -0300

    Create vetornatela.js

---
+++ js/curso-em-video/aula15/vetornatela.js
@@ -0,0 +1,9 @@
+let num = [2, 5, 6, 7, 9]
+
+/*for (pos = 0; pos < num.length; pos++){
+    console.log(`a posição ${pos} tem valor ${num[pos]}`)
+} */
+
+for (let pos in num){
+    console.log()
+}
