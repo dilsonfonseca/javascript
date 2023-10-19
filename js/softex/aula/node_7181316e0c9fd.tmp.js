4305f1c550483ea6cefeacdecd71918682e4d4d4
Dilson Fonseca <135386113+dilsonfonseca@users.noreply.github.com>
2023-10-19 16:43:51 -0300

    .

---
+++ js/softex/aula/node_7181316e0c9fd.tmp.js
@@ -0,0 +1,7 @@
+let Dilson = {
+    cor: "parda";
+    idade: 26;
+    corCabelo: "preto";
+}
+
+console.log(Dilson.cor)
---
+++ js/softex/aula/orientacaoobjs.js
@@ -0,0 +1,13 @@
+/*let dilson = {
+    cor: "parda",
+    idade: 26,
+    corCabelo: "preto",
+}
+
+console.log(dilson.cor)*/
+
+function dilson(cor, idade, corCabelo){
+    this.cor = "pardo"
+    this.idade = 26
+    this.corCabelo = "preto"
+}
