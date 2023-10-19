493365d19dc5a463b682407547b52f5f345dd915
Dilson Fonseca <135386113+dilsonfonseca@users.noreply.github.com>
2023-10-19 17:16:49 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
-let n1 = prompt("Digite um número")
-let n2 = prompt("Digite outro número")
-let result
-
-if (n2 > n1){
-    console.log("Escreva novamente")
-} else{
-    result = parseInt(n1/n2 * 100 + "%")
-}
-console.log(result)
+function soma(num1, num2){
+    soma = num1 + num2
+    return soma
+  }
+  
+  function multiplicacao(num1, num2){
+    multiplicacao = num1*num2
+    return multiplicacao
+  }
+  
+  function subtracao(num1, num2){
+    subtracao = num1 - num2
+    return subtracao
+  }
+  
+  function divisao(num1, num2){
+    divisao = num1/num2
+    return divisao
+  }
+  
+  
+  switch (2,3) {
+    case '1':
+      soma(2,3)
+      break;
+      case '2':
+      subtracao(3,2)
+      break;
+      case '3':
+      multiplicacao(2,3)
+      break;
+      case '4':
+      divisao(2,3)
+      break;
+    default:
+      console.log("Escolha uma das opções")
+  }
