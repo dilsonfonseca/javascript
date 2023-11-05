f9dce4fe52ba344b5467184a3371f5b6c8aa9b25
Dilson <dilsonfonseca97@gmail.com>
2023-11-05 19:07:04 -0300

    Create m5e2.js

---
+++ js/softex/plataforma/m5e2.js
@@ -0,0 +1,16 @@
+try {
+  let numero1 = parseFloat(prompt("Digite o primeiro número:"));
+  let numero2 = parseFloat(prompt("Digite o segundo número:"));
+
+  if (numero2 === 0) {
+    throw new Error("Divisão por zero não é permitida.");
+  }
+
+  let resultado = numero1 / numero2;
+
+  console.log(`O resultado da divisão é: ${resultado}`);
+} catch (erro) {
+  console.error(`Ocorreu um erro: ${erro.message}`);
+} finally {
+  console.log("Operação finalizada.");
+}
