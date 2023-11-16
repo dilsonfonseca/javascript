0d384a3c6da0e5a0d7d25879c504b7953d0841e1
Dilson <dilsonfonseca97@gmail.com>
2023-11-16 20:55:36 -0300

    .

--- js/origamid/block002/node_ddd6514026ff8.tmp.js
+++
@@ -1,6 +0,0 @@
-console.log('eae')
-
-var preco = 20
-var totalComprado = 5
-precoTotal = preco * totalComprado
-console.log(precoTotal)
---
+++ js/origamid/block002/numeros/main.js
@@ -0,0 +1,5 @@
+var total = 10 + 5 + 10
+var divisao = 200 / 5
+var modulo = 454878746514564563 % 3
+
+console.log(modulo)
