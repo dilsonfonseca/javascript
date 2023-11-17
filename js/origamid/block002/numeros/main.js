707d96c83a4df4049c725d0d9fbec3988622cad8
Dilson <dilsonfonseca97@gmail.com>
2023-11-16 21:08:52 -0300

    Update main.js

--- js/origamid/block002/numeros/main.js
+++ js/origamid/block002/numeros/main.js
@@ -3,3 +3,10 @@ var divisao = 200 / 5
 var modulo = 454878746514564563 % 3
 
 console.log(modulo)
+
+var testeNaN = '100' / 2
+console.log(isNaN(testeNaN))
+
+var x = 5
+console.log(++x)
+console.log(x)
