2beff0b7ef3d11b0ffddf948f509a738a70a11da
Dilson <dilsonfonseca97@gmail.com>
2023-11-21 23:27:04 -0300

    .

---
+++ js/origamid/js-6-completo/arrays-e-loops/main.js
@@ -0,0 +1,11 @@
+var videoGames = ['Switch', 'PS4', 'Xbox']
+
+console.log(videoGames[1])
+console.log(videoGames.pop())
+console.log(videoGames)
+console.log(videoGames.push('3DS'))
+console.log(videoGames)
+
+for (let numero = 0; numero <= 10; numero++){
+    console.log(numero)
+}
