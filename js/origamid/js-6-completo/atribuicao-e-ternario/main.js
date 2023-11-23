3fb9044766f5c11f522beb84327ee6cd9fc55c77
Dilson <dilsonfonseca97@gmail.com>
2023-11-23 20:37:31 -0300

    .

---
+++ js/origamid/js-6-completo/atribuicao-e-ternario/main.js
@@ -0,0 +1,18 @@
+var numero = 20
+numero += 10 // numero = numero + 10  --- + - / * ** %
+console.log(numero)
+
+// Operador Ternário - ternário pois tem três operações - condição ? true :  false
+
+var idade = 19
+var naoPossuiDiabetes = true
+var podeBeber = (idade >= 18 && naoPossuiDiabetes) ? 'Pode beber' : 'Não pode beber'
+console.log(podeBeber)
+
+//If abreviado
+
+var possuiFaculdade = true
+if(possuiFaculdade) 
+console.log('possui faculdade') 
+else console.log('Não possui faculdade')
+
---
+++ js/origamid/js-6-completo/escopo/main.js
@@ -0,0 +1,21 @@
+let carro = 'Fusca'
+function mostrarCarro(){
+    console.log(carro)
+}
+
+mostrarCarro()
+console.log(carro)
+
+
+const semana ='Sexta'
+console.log(semana)
+
+const data = {
+    dia: 28,
+    mes: 'Dezembro',
+    ano: 2018,
+}
+
+data.dia = 29
+console.log(data.dia)
+
